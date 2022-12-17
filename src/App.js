0254e096f06ac1f08cbc82838c54e7d4b9d60372
ANJANA1732 <anjusivakumar17302@gmail.com>
2022-12-17 15:36:46 +0530

    commit msg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.css';
+import InputGroup from 'react-bootstrap/InputGroup'
+import  Alert  from 'react-bootstrap/Alert';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Container>
+    <div>
+      <h1 className="title">Phishing URL checker</h1>
+    
     </div>
-  );
+
+    <div className="content">
+
+    <Form.Label htmlFor="basic-url">Enter URL</Form.Label>
+      <InputGroup className="mb-3">
+        <Form.Control id="basic-url" aria-describedby="basic-addon3"/>
+      </InputGroup>
+      
+      <Row>
+        
+        <Col>
+        
+        <Button variant="secondary" size="lg">Reset</Button>{' '}
+        </Col>
+        
+        <Col>
+      <Button variant="success" size="lg">Submit</Button>{' '}
+      
+      </Col>
+      </Row>
+      </div>
+   
+    
+ 
+    </Container>
+  )
 }
 
 export default App;
