c101e5f2ddeda453f91629ae1d56d13d6d6a251f
ANJANA1732 <anjusivakumar17302@gmail.com>
2022-12-18 09:10:34 +0530

    commit_2

--- react/src/App.js
+++ react/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
@@ -6,42 +6,44 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.css';
-import InputGroup from 'react-bootstrap/InputGroup'
-import  Alert  from 'react-bootstrap/Alert';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Alert from 'react-bootstrap/Alert';
 
 function App() {
+  const [url, setUrl] = useState('');
+
+  function handleReset() {
+    setUrl('');
+  }
+
   return (
     <Container>
-    <div>
-      <h1 className="title">Phishing URL checker</h1>
-    
-    </div>
-
-    <div className="content">
-
-    <Form.Label htmlFor="basic-url">Enter URL</Form.Label>
-      <InputGroup className="mb-3">
-        <Form.Control id="basic-url" aria-describedby="basic-addon3"/>
-      </InputGroup>
-      
-      <Row>
-        
-        <Col>
-        
-        <Button variant="secondary" size="lg">Reset</Button>{' '}
-        </Col>
-        
-        <Col>
-      <Button variant="success" size="lg">Submit</Button>{' '}
-      
-      </Col>
-      </Row>
+      <div>
+        <h1 className="title">Phishing URL checker</h1>
       </div>
-   
-    
- 
+
+      <div className="content">
+        <Form.Label htmlFor="basic-url">Enter URL</Form.Label>
+        <InputGroup className="mb-3">
+          <Form.Control
+            id="basic-url"
+            aria-describedby="basic-addon3"
+            value={url}
+            onChange={e => setUrl(e.target.value)}
+          />
+        </InputGroup>
+
+        <div className="box">
+          <Button variant="secondary" size="lg" onClick={handleReset}>
+            Reset
+          </Button>{' '}
+          <Button variant="success" size="lg">
+            Submit
+          </Button>{' '}
+        </div>
+      </div>
     </Container>
-  )
+  );
 }
 
 export default App;
